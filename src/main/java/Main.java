f9b1e65c1102bf686e648e2573a562d9a48d4bdf
wiksell91 <victor.wiksell@gmail.com>
2022-05-10 11:24:34 +0200

    Snyggat till

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
-    public static final int ROWS = 10;
-    public static final int COLS = 20;
-    public static final int GAME_SPEED = 1000;
-
-
     public static void main(String[] args) {
+        final int ROWS = 10;
+        final int COLS = 20;
+        final int GAME_SPEED = 1000;
+
+
         Board board = new Board(ROWS, COLS);
         Board newBoard = new Board(ROWS, COLS);
         Cells cells = new Cells();
